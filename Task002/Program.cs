1d5fa17ac7cd0e3735dc0beccd7fda7ca76ade1f
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-04-20 21:28:00 +0300

    готовое решение задачи 66 с комментариями

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -5,8 +5,27 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.Write("Введите число M: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
+if (n >= 1 && m >= 1)                                     // проверка чисел на натуральность
+{
+  SumOfElements(n, m);
+  Console.WriteLine($"Сумма промежуточных элементов равна: {SumOfElements(n, m)}");
+}
+else
+{
+  Console.WriteLine("Вы ввели НЕ ВЕРНОЕ число! Повторите ввод.");
+}
 
+int SumOfElements(int n, int m)                           // рекурсивный метод подсчёта суммы промежуточных элементов
+{
+  if (n == m) return n;
+  if (n > m) return SumOfElements(m + 1, n) + m;
+  else return SumOfElements(n + 1, m) + n;
+}
 
 
 
