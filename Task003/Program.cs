99ce47b63dc4d584fd3b9fd59ac51f070a777e6a
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-04-24 19:54:11 +0300

    решение задачи 68

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -4,26 +4,24 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 Console.Clear();
-int Prompt(string message)
-{
-  Console.Write(message);
-  int result = Convert.ToInt32(Console.ReadLine());
-  return result;
-}
+int m = ReadNumberFromConsole("Введите значение m: ");
+int n = ReadNumberFromConsole("Введите значение m: ");
+Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
 
-int Akkerman(int m, int n)
+int Akkerman(int m, int n)                              
 {
   if (m == 0) return n + 1;
   if (m > 0 && n == 0) return Akkerman(m - 1, 1);
   else return Akkerman(m - 1, Akkerman(m, n - 1));
 }
 
-int m = Prompt("Input M: ");
-int n = Prompt("Input N: ");
-
-Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
-
-
+int ReadNumberFromConsole(string message = "")          // метод для чтения из консоли введённого значения
+{
+  if (message != "")
+    Console.WriteLine(message);
+  string input = Console.ReadLine();
+  return int.Parse(input);
+}
 
 
 
